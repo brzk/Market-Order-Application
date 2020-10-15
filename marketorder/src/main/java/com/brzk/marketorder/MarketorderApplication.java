8a43264ce0c8c208aa60dc4f3c2db8cde9e2551f
Barak <Barakraz6@gmail.com>
2020-10-15 11:09:19 +0300

    Create new Java Spring Boot application for Market-Order-Application

---
+++ marketorder/src/main/java/com/brzk/marketorder/MarketorderApplication.java
@@ -0,0 +1,13 @@
+package com.brzk.marketorder;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MarketorderApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MarketorderApplication.class, args);
+	}
+
+}
---
+++ marketorder/src/test/java/com/brzk/marketorder/MarketorderApplicationTests.java
@@ -0,0 +1,13 @@
+package com.brzk.marketorder;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MarketorderApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
